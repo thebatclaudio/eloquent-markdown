deb917742aea9c8558220e15a524873b44ca8a2c
Claudio La Barbera <claudio.labarbera@everli.com>
2024-05-15 18:21:01 +0200

    feat: create new MarkdownWithDateModel

---
+++ src/Models/MarkdownWithDateModel.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheBatClaudio\EloquentMarkdown\Models;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+abstract class MarkdownWithDateModel extends MarkdownModel
+{
+    public static function extractDate(string $filePath): Carbon
+    {
+        preg_match("/\d{4}-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])/", $filePath, $date);
+
+        return new Carbon($date[0]);
+    }
+
+    public static function extractSlug(string $filePath): string
+    {
+        return Str::substr(static::extractFileId($filePath), 11);
+    }
+
+    protected static function extractAttributes(string $filePath): array
+    {
+        return array_merge(
+            parent::extractAttributes($filePath),
+            [
+                'date' => static::extractDate($filePath),
+                'slug' => self::extractSlug($filePath),
+            ]
+        );
+    }
+}
